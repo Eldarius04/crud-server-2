f0ab3593f6e456543ab1771253c90b4cce9056ba
Neto test <eldar-guseynov-1984@mail.ru>
2025-06-19 11:12:07 +0300

    Commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,37 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostRepository {
+  private final AtomicLong counter = new AtomicLong(0);
+  private final ConcurrentHashMap<Long, Post> storage = new ConcurrentHashMap<>();
+
+  public List<Post> all() {
+    return new ArrayList<>(storage.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(storage.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      long id = counter.incrementAndGet();
+      post.setId(id);
+      storage.put(id, post);
+      return post;
+    } else {
+      return storage.computeIfPresent(post.getId(), (key, oldPost) -> post);
+    }
+  }
+
+  public void removeById(long id) {
+    storage.remove(id);
+  }
+}
---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import com.google.gson.Gson;
+import ru.netology.repository.PostRepository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+  private final PostRepository repository;
+
+  @Autowired
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+public class PostService {
+  private final PostRepository repository;
+  private final Gson gson = new Gson();
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public String all() {
+    return gson.toJson(repository.all());
+  }
+
+  public String getById(long id) {
+    return gson.toJson(repository.getById(id).orElse(null));
+  }
+
+  public String save(Reader body) throws IOException {
+    try (BufferedReader reader = new BufferedReader(body)) {
+      StringBuilder sb = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        sb.append(line);
+      }
+      var post = gson.fromJson(sb.toString(), Post.class);
+      return gson.toJson(repository.save(post));
+    }
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
