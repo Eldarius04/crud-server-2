30d1691bdd113d54325ed9252373208c888dae32
Neto test <eldar-guseynov-1984@mail.ru>
2025-06-19 11:12:07 +0300

    Commit

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,53 @@
+package ru.netology.servlet;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+  public static final String API_POSTS = "/api/posts";
+  public static final String API_POSTS_ID = "/api/posts/\\d+";
+
+  @Override
+  public void init() {
+    ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+    controller = context.getBean(PostController.class);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+
+      if (method.equals("GET") && path.equals(API_POSTS)) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals("GET") && path.matches(API_POSTS_ID)) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals("POST") && path.equals(API_POSTS)) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals("DELETE") && path.matches(API_POSTS_ID)) {
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
