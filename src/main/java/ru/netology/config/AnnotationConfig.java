08da99c44403eec8295aa3a1c093da9d34207bbe
Neto test <eldar-guseynov-1984@mail.ru>
2025-06-25 23:11:36 +0300

    Add AnnotationConfig for Spring DI

--- src/main/java/ru/netology/config/AnnotationConfig.java
+++ src/main/java/ru/netology/config/AnnotationConfig.java
@@ -1,9 +1,9 @@
-package ru.netology;
+package ru.netology.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("ru.netology")
-public class AppConfig {
+public class AnnotationConfig {
 }
